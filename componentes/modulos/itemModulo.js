1290c9d666978b39638aacfc8bc09720cf03f0e6
Mar Lopez <mhlopez@scl.edu.gt>
2025-08-19 21:45:09 -0600

    actualizado

--- componentes/modulos/itemModulo.js
+++ componentes/modulos/itemModulo.js
@@ -1,17 +1,35 @@
-export function item(titulo,urlImg) {
+export function item(titulo,urlImg,price,description) {
     
     let item = document.createElement('div');
     item.className = "item";
-    item.textContent = "titulo"
+
+    let h3 = document.createElement('h3');
+    h3.className = "titulo";
+    h3.textContent = titulo;
+    item.appendChild(h3);
+    
 
     let img = document.createElement('img');
     img.src = urlImg;
     item.appendChild(img);
 
+    
+    // elemento descripción
+    let descripcion = document.createElement('p');
+    descripcion.className = "description";
+    descripcion.textContent = description;
+    item.appendChild(descripcion);
+    
+    let precioEl = document.createElement('p');
+    precioEl.className = "price";
+    precioEl.textContent = `Precio: $${price}`;
+    item.appendChild(precioEl);
+
+
     item.addEventListener('click' ,()=>{
         item.classList.add("verde");
-        let rHeader = document.querySelector('header');
-        rHeader.classList.add("ocultar");
+        let headercontenedor = document.querySelector('header');
+        headercontenedor.classList.add("ocultar");
     });
 
     return item;
